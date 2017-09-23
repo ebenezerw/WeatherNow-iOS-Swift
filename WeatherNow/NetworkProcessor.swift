2ace929d85b69d102829e12dc99cfcfb2d1550a9
Ebenezer Williams <ebenezerw@me.com>
2017-09-23 12:52:24 -0400

    Initial Commit

---
+++ WeatherNow/NetworkProcessor.swift
@@ -0,0 +1,66 @@
+//
+//  NetworkProcessor.swift
+//  WeatherNow
+//
+//  Created by Ebenezer Williams on 9/23/17.
+//  Copyright © 2017 Ebenezer Williams. All rights reserved.
+//
+
+import Foundation
+
+class NetworkProcessor
+{
+    lazy var configuration: URLSessionConfiguration = URLSessionConfiguration.default
+    lazy var session: URLSession = URLSession(configuration: self.configuration)
+    
+    let url: URL
+    
+    init(url: URL)
+    {
+        self.url = url
+    }
+    
+    typealias JSONDictionaryHandler = (([String : Any]?) -> Void)
+    
+    func downloadJSONFromURL (_ completion: @escaping JSONDictionaryHandler){
+        let request = URLRequest(url: self.url)
+        let dataTask = session.dataTask(with: request) {(data, response, error ) in
+            
+            if error == nil {
+                if let httpResponse = response as? HTTPURLResponse {
+                    switch httpResponse.statusCode {
+                    case 200:
+                        //successful response
+                        if let data = data {
+                            print(data)
+                        }
+                        
+                    default:
+                        print("HTTP Response Code: \(httpResponse.statusCode)")
+                    }
+                }
+            } else {
+                print ("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
